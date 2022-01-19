713919b76f5ed5819887826a7ece23ae3cf23080
iEmbrion <garyang92@hotmail.com>
2022-01-19 14:08:07 +0800

    Changed axios baseURL from localhost to prod (heroku)

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000',
+  // baseURL: 'http://localhost:8000',
+  baseURL: 'https://tinder-backend-gary.herokuapp.com',
 });
 
 export default instance;
